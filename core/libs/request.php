31574616b915604266301cb179b1796fa73ea944
ruansheng <825307513@qq.com>
2015-06-17 22:07:22 +0800

    write request

--- core/libs/request.php
+++ core/libs/request.php
@@ -7,4 +7,95 @@
  */
 class Request {
 
+    /**
+     * GET POST REQUEST
+     * @var array
+     */
+    static private $request = array();
+
+    /**
+     * Filter type
+     * @var array
+     */
+    static private $filter = array(
+        'htmlentities',
+        'addslashes'
+    );
+
+    /**
+     * HTTP GET
+     * @param $key
+     * @param string $default
+     * @return array
+     */
+    public static function get($key='', $default = '') {
+        $tmp = '';
+        if($key == '') {
+            $tmp = self::_filter($_GET);
+        } else {
+            $tmp =  self::_filter($_GET[$key]);
+        }
+
+        if(!empty($default)) {
+            if(empty($tmp)) {
+                $tmp = $default;
+            }
+        }
+        return $tmp;
+    }
+
+    /**
+     * HTTP POST
+     * @param $key
+     * @param string $default
+     * @return array
+     */
+    public static function post($key='', $default = '') {
+        if($key == '') {
+            return self::_filter($_POST);
+        } else {
+            return self::_filter($_POST[$key]);
+        }
+    }
+
+    /**
+     * Filter
+     * @param $data
+     * @return string
+     */
+    private static function _filter($data) {
+        $tmp = '';
+        if(!empty($data)){
+            if(empty(self::$filter)) {
+                $tmp = $data;
+            } else {
+                foreach(self::$filter as $filter) {
+                    if(is_array($data)) {
+                        $tmp = self::_loopFilter($data);
+                    } else {
+                        $tmp = $filter($data);
+                    }
+                }
+            }
+        }
+        return $tmp;
+    }
+
+    /**
+     * loop filter
+     * @param $data
+     * @return array
+     */
+    private static function _loopFilter($data) {
+        $tmp = array();
+        foreach($data as $key => $i) {
+            if(is_array($i)) {
+                $tmp[$key] = self::_loopFilter($i);
+            } else {
+                $tmp[$key] = $i;
+            }
+        }
+        return $tmp;
+    }
+
 }
