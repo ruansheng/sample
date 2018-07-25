421286172c209c95c96d227885f4964f19f563ca
阮胜 <ruansheng@qingsongchou.com>
2018-07-25 10:43:57 +0800

    update

--- apps/rpc/init_rpc.php
+++ apps/rpc/init_rpc.php
@@ -5,3 +5,5 @@ require_once APPS_DIR . '/rpc/config/config.php';
 
 // require app controllers
 require_once APPS_DIR . '/rpc/controllers/base_controller.php';
+
+require_once APPS_DIR . '/rpc/router.php';
--- apps/web/init_web.php
+++ apps/web/init_web.php
@@ -2,3 +2,5 @@
 
 // require app controllers
 require_once APPS_DIR . '/web/controllers/base_controller.php';
+
+require_once APPS_DIR . '/web/router.php';
