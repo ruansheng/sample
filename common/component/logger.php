86fbf95ee28e34a0599935f98baa73b7fadeb86c
阮胜 <ruansheng@qingsongchou.com>
2018-09-29 13:07:48 +0800

    fix

--- common/component/logger.php
+++ common/component/logger.php
@@ -8,10 +8,6 @@
  * @return bool
  */
 function errorHandler($errno, $errstr, $errfile, $errline) {
-    if (!(error_reporting() & $errno)) {
-        return false;
-    }
-
     $request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
     switch ($errno) {
         case E_USER_ERROR:
@@ -35,7 +31,7 @@ function errorHandler($errno, $errstr, $errfile, $errline) {
     $request_uri_content = $request_uri ? sprintf('   [REQUEST_URI:%s]', $request_uri) : '   [REQUEST_URI: Unkown]';
     $text = sprintf("[%s %s] %s  %s in %s  on line %s %s \n", date('d-M-Y H:i:s'), $timezone, $error_type, $errstr, $errfile, $errline, $request_uri_content);
 
-    $log_file = isset($GLOBALS['log_file']) ? $GLOBALS['log_file'] : '/var/log/php.fpm.log';
+    $log_file = isset($GLOBALS['log_file']) ? $GLOBALS['log_file'] : '/var/log/php-fpm.log';
     if(is_writeable($log_file)) {
         file_put_contents($log_file, $text, FILE_APPEND);
     }
